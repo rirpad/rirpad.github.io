c234975e71e04b00e8c5571606f9469300bb11e1
rirpad <sahrirpad@yahoo.co.id>
2023-10-04 13:51:15 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,350 @@
+ <!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+	<!--AOS -->
+	  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+	<!-- Akhir AOS -->
+	
+	<!-- My CSS -->
+	<link rel="stylesheet" href="style.css"/>
+	<title>Webku</title>
+  </head>
+  <body id="home">
+  <!-- Awal Navbar-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">MUHAMMAD SAHRIR</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#projects">Portfolio</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="#my-keyboards">My Photo</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="#gallery">Gallery</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- Akhir Navbar-->
+
+<!-- jumbtron-->
+<section class="jumbotron text-center">
+	<img src="images/sahrir3.jpg" alt="Muhammad Sahrir" width="200" class="rounded-circle img-thumbnail">
+  <h1 class="display-4">Muhammad Sahrir</h1>
+		<p class="lead"></p>
+		
+		<p class="fs-5 " style="line-height:20px;"><b>Akhir Pengabdian ASN Menjabat Sebagai Kepala Balai Karantina Pertanian Gorontalo (Tahun 2021 - 2023)</b><br>
+		 Pernah Menjabat Sebagai Kepala Bagian Umum Balai Besar Karantina Pertanian Makassar<br>
+		 Pernah Menjabat Sebagai Kepala Bidang Operasional Balai Besar Karantina Pertanian Tanjung Perak Surabaya<br>
+		 Pernah Menjabat Sebagai Kepala Bidang Pengawasan dan Penindakan  Balai Besar Karantina Pertanian Surabaya<br>
+		 Pernah Menjabat Sebagai Kepala Bidang Pengawasan dan Penindakan  Balai Besar Karantina Pertanian Makassar<br>
+		 Pernah Menjabat Sebagai Kepala Bidang Karantina Tumbuhan Balai Besar Karantina Pertanian Makassar<br>
+		 Pernah Menjabat Sebagai Kepala Seksi Informasi dan Sarana Teknis Balai Karantina Pertanian Kelas I Makassar<br>
+		 Pernah Menjabat Sebagai Kepala Sub Seksi Data dan Informasi Balai Karantina Tumbuhan Makassar<br>
+		 Pernah Menjabat Sebagai Fungsional Pengendali Organisme Pengganggu Tumbuhan (POPT) Muda Balai Karantina Tumbuhan Makassar<br>
+		 Pernah Menjabat Sebagai Penanggung Jawab/Membuka Wilker Karantina Pertanian Paotere Balai Karantina Tumbuhan Makassar<br>
+		 Pernah Menjabat Sebagai Penanggung Jawab Wilker Karantina Pertanian BajoE Balai Karantina Tumbuhan Makassar<br>
+		 Pernah Menjabat Sebagai Pejabat Pembuat Komitmen Selama (PPK) 10 Tahun (2008 sd/ 2017)<br>
+		 Pernah Menjabat Sebagai Manajer Mutu Laboratorium Balai Besar Karantina Pertanian Makassar<br>
+		 Pernah Menjabat Sebagai Ketua Satlak PI Balai Besar Karantina Pertanian Makassar<br>
+		 Pernah Menjabat Sebagai Ketua Agen Perubahan Untuk Reformasi Birokrasi Balai Besar Karantina Pertanian Makassar<br>
+		 Pernah Menjabat Sebagai Koordinator Implementasi WBK/WBBM Balai Besar Karantina Pertanian Makassar<br>
+		 Pernah Menjabat Sebagai Koordinator PPID/Humas Balai Besar Karantina Pertanian Makassar</p>
+   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,96L26.7,96C53.3,96,107,96,160,85.3C213.3,75,267,53,320,69.3C373.3,85,427,139,480,165.3C533.3,192,587,192,640,165.3C693.3,139,747,85,800,85.3C853.3,85,907,139,960,160C1013.3,181,1067,171,1120,154.7C1173.3,139,1227,117,1280,101.3C1333.3,85,1387,75,1413,69.3L1440,64L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path></svg>
+</section>
+
+<!-- About -->
+<section id="about">
+	<div class="container">
+		  <div class="row text-center mb-3">
+			<div class="col">
+			  <h2>About Me</h2>
+			</div>
+		  </div>
+		  <div class="row justify-content-center fs-5 text-center">
+			<div class="col-md-4" data-aos="fade-right">
+			  <p>Bahwa pada saat kami menyusun Aplikasi ini ternyata tidak semudah apa yang kami bayangkan, ternyata diperlukan ketelitian dan ketekunan yang memadai menurut standar kami. Terus terang selama menyusun Aplikasi ini, hanyalah niat dan semangat untuk belajar membuat aplikasi dengan harapan untuk memudahkan/membantu kami dalam bekerja.</p>
+			</div>
+			<div class="col-md-4" data-aos="fade-left" data-aos-delay="200">
+			  <p>Kami percaya masih banyak kekurangan dalam Aplikasi ini. Walaupun demikian, besar harapan kami, kiranya aplikasi ini memberikan manfaat bagi teman-teman Tim Semua. Barakallah Aamiin.</p>
+			</div>
+		  </div>
+	</div>
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e2edff" fill-opacity="1" d="M0,160L26.7,138.7C53.3,117,107,75,160,90.7C213.3,107,267,181,320,208C373.3,235,427,213,480,192C533.3,171,587,149,640,149.3C693.3,149,747,171,800,197.3C853.3,224,907,256,960,266.7C1013.3,277,1067,267,1120,240C1173.3,213,1227,171,1280,160C1333.3,149,1387,171,1413,181.3L1440,192L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path></svg>
+</section>
+<!-- Akhir About -->
+
+<!-- projects -->
+<section id="projects">
+	<div class="container">
+		  <div class="row text-center mb-3">
+			<div class="col">
+			  <h2>Portfolio</h2>
+			</div>
+		  </div>
+		  <div class="row justify-content-center fs-5 text-center">
+			<div class="col-md-4 mb-3">
+			  <div class="card" data-aos="flip-left" data-aos-duration="500">
+				  <img src="images/rirkel.jpg" class="card-img-top" alt="projects1">
+				  <div class="card-body">
+					<p class="card-text">Bersama Keluargaku</p>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-4 mb-3">
+			  <div class="card" data-aos="flip-left" data-aos-duration="500" data-aos-delay="100">
+				  <img src="images/tvtani.jpg" class="card-img-top" alt="projects2">
+				  <div class="card-body">
+					<p class="card-text">Wawancara TV TANI Juara WBK dan SPI</p>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-4 mb-3">
+			  <div class="card" data-aos="flip-left" data-aos-duration="500" data-aos-delay="200">
+				  <img src="images/spi.jpg" class="card-img-top" alt="projects3">
+				  <div class="card-body">
+					<p class="card-text">Bimbingan Teknis SPI, Inspektur IV Itjen Kementan</p>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-4 mb-3">
+			  <div class="card" data-aos="flip-left" data-aos-duration="500" data-aos-delay="300">
+				  <img src="images/rudy01.jpg" class="card-img-top" alt="projects4">
+				  <div class="card-body">
+					<p class="card-text">Foto Wisuda Rudy Fachri Muswar</p>
+				  </div>
+				</div>
+			</div>
+			
+			<div class="col-md-4 mb-3">
+			  <div class="card" data-aos="flip-left" data-aos-duration="500" data-aos-delay="400">
+				  <img src="images/rudy02.jpg" class="card-img-top" alt="projects5">
+				  <div class="card-body">
+					<p class="card-text">Foto Wisuda Rudy Fachri Muswar</p>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-4 mb-3">
+			  <div class="card" data-aos="flip-left" data-aos-duration="500" data-aos-delay="500">
+				  <img src="images/rudy03.jpg" class="card-img-top" alt="projects5">
+				  <div class="card-body">
+					<p class="card-text">Foto Wisuda Rudy Fachri Muswar</p>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-4 mb-3">
+			  <div class="card" data-aos="flip-left" data-aos-duration="500" data-aos-delay="600">
+				  <img src="images/rudy04.jpg" class="card-img-top" alt="projects5">
+				  <div class="card-body">
+					<p class="card-text">Foto Wisuda Rudy Fachri Muswar</p>
+				  </div>
+				</div>
+			</div>
+		  </div>
+	 
+	</div>
+	
+	<section id="my-keyboards">
+		<div class="container">
+			<div class="row text-center mb-3">
+			<div class="col">
+			  <h2>My Photo</h2>
+			</div>
+		  </div>
+		  <div class="row">
+			<div class="col-md-6 col-lg-4 mb-3">
+				 <div class="keyboard-box">
+					<h4 class="keyboard-name">Muhammad Sahrir</h4>
+					<img src="images/sahrir.png" alt="sahrir" class="keyboard-img">
+					<a href="#" class="btn btn-light keyboard-detail-button">Show Details</a>
+				 </div>
+			</div>
+			
+			<div class="col-md-6 col-lg-4 mb-3">
+				 <div class="keyboard-box">
+					<h4 class="keyboard-name">Rudy Fachri Muswar</h4>
+					<img src="images/rudy01.jpg" alt="rudy" class="keyboard-img">
+					<a href="#" class="btn btn-light keyboard-detail-button">Show Details</a>
+				 </div>
+			</div>
+			
+			<div class="col-md-6 col-lg-4 mb-3">
+				 <div class="keyboard-box">
+					<h4 class="keyboard-name">Rudy Fachri Muswar</h4>
+					<img src="images/rudy02.jpg" alt="rudy" class="keyboard-img">
+					<a href="#" class="btn btn-light keyboard-detail-button">Show Details</a>
+				 </div>
+			</div>
+			
+			<div class="col-md-6 col-lg-4 mb-3">
+				 <div class="keyboard-box">
+					<h4 class="keyboard-name">Rudy Fachri Muswar</h4>
+					<img src="images/rudy03.jpg" alt="rudy" class="keyboard-img">
+					<a href="#" class="btn btn-light keyboard-detail-button">Show Details</a>
+				 </div>
+			</div>
+			
+			<div class="col-md-6 col-lg-4 mb-3">
+				 <div class="keyboard-box">
+					<h4 class="keyboard-name">Rudy Fachri Muswar</h4>
+					<img src="images/rudy04.jpg" alt="rudy" class="keyboard-img">
+					<a href="#" class="btn btn-light keyboard-detail-button">Show Details</a>
+				 </div>
+			</div>
+			
+			<div class="col-md-6 col-lg-4 mb-3">
+				 <div class="keyboard-box">
+					<h4 class="keyboard-name">Logo Resmi Barantin</h4>
+					<img src="images/logoresmi1.png" alt="logo" class="keyboard-img">
+					<a href="#" class="btn btn-light keyboard-detail-button">Show Details</a>
+				 </div>
+			</div>
+			
+		  </div>
+		</div>
+	</section>
+	 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,64L34.3,74.7C68.6,85,137,107,206,133.3C274.3,160,343,192,411,181.3C480,171,549,117,617,101.3C685.7,85,754,107,823,96C891.4,85,960,43,1029,48C1097.1,53,1166,107,1234,144C1302.9,181,1371,203,1406,213.3L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg>
+</section>
+<!-- Akhir Projects -->
+
+<!-- gelery -->
+<section id="gallery">
+	<div class="container">
+		<div class="row text-center mb-3">
+			<div class="col">
+			  <h2>Gellery</h2>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<div class="col-md-3">
+			  <a href="#">
+			  <img src="images/project/foto1.jpg" alt="foto1" class="img-fluid gallery-img">
+			  </a>
+			</div>
+			<div class="col-md-3">
+			  <a href="#">
+			  <img src="images/project/foto2.jpg" alt="foto2" class="img-fluid gallery-img">
+			  </a>
+			</div>
+			<div class="col-md-3">
+			  <a href="#">
+			  <img src="images/project/foto3.jpg" alt="foto3" class="img-fluid gallery-img">
+			  </a>
+			</div>
+			<div class="col-md-3">
+			  <a href="#">
+			  <img src="images/project/foto4.jpg" alt="foto4" class="img-fluid gallery-img">
+			  </a>
+			</div>
+			<div class="col-md-3">
+			  <a href="#">
+			  <img src="images/project/foto6.jpg" alt="foto6" class="img-fluid gallery-img">
+			  </a>
+			</div>			
+		</div>
+		
+	</div>
+
+</section>
+<!-- Akhir Projects -->
+
+<!-- contact -->
+<section id="contact">
+	<div class="container">
+		  <div class="row text-center mb-3">
+			<div class="col">
+			  <h2>Contact Me</h2>
+			</div>
+		  </div>
+		  <div class="row justify-content-center">
+			<div class="col-md-6">
+			   <form>
+				  <div class="mb-3">
+					<label for="name" class="form-label">Nama Lengkap</label>
+					<input type="text" class="form-control" id="name" aria-describedby="name">
+				  </div>
+				   
+				  <div class="mb-3">
+					<label for="email" class="form-label">Email</label>
+					<input type="email" class="form-control" id="email" aria-describedby="email">
+				  </div> 
+				  <div class="mb-3">
+					<label for="pesan" class="form-label">Pesan</label>
+					<textarea class="form-control" id="pesan" rows="3"></textarea>
+				  </div> 
+				  <button type="submit" class="btn btn-primary">Submit</button>
+				</form>
+			</div>
+		  </div>
+	</div>
+	 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#2368F5" fill-opacity="1" d="M0,128L26.7,149.3C53.3,171,107,213,160,229.3C213.3,245,267,235,320,197.3C373.3,160,427,96,480,96C533.3,96,587,160,640,197.3C693.3,235,747,245,800,234.7C853.3,224,907,192,960,192C1013.3,192,1067,224,1120,234.7C1173.3,245,1227,235,1280,213.3C1333.3,192,1387,160,1413,144L1440,128L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path></svg>
+</section>
+<!-- Akhir contact -->
+
+<!-- Akhir footer-->
+<footer class="bg-primary text-white fw-bold text-center pb-3">
+<p>
+Created With Love By Muhammad Sahrir
+</p>
+</footer> 
+<!-- Akhir footer-->	
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+	
+<!-- AOS-->		
+  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+  const galleryImage = document.querySelectorAll('.gallery-img');
+  galleryImage.forEach((img,i) => {
+	  img.dataset.aos = 'fade-down';
+	  img.dataset.aosDelay = i * 100;
+	  img.dataset.aosDuration = 1000;
+  })
+    AOS.init({
+		once: true,
+		duration: 2000,
+	});
+  </script>	
+<!-- Akhir AOS-->		
+	
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/TextPlugin.min.js"></script> 
+<script>
+gsap.registerPlugin(TextPlugin);
+gsap.to('.lead',{duration: 2, delay: 1.5, text:'ASSALAMU ALAIKUM WARAHMATULLAHI WABARAKATUH'});
+gsap.from('.jumbotron img',{duration: 1, rotateY: 360, opacity:0});
+gsap.from('.navbar',{duration: 1.5, y: '-100%', opacity:0, ease:'bounce'});
+gsap.from('.display-4',{duration: 1, x: -50, opacity:0,delay: 0.5, ease:'back'});
+</script>
+<script type="text/javascript" src="js/vanilla-tilt.min.js"></script>
+<script type="text/javascript">
+	VanillaTilt.init(document.querySelectorAll(".keyboard-box"), {
+		max: 90,
+		speed: 2000,
+		scale: 1.1,
+		glare: true,
+	});
+	
+	 
+</script>
+</body>
+</html>
